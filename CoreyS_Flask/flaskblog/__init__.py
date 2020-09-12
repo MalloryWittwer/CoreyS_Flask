be9d8c40b07b291f5d466b2741e81d52efd2e40b
MalloryWittwer <mallory.wittwer@gmail.com>
2020-09-12 19:00:04 +0800

    All updates till section 10 of the tutorial.

--- CoreyS_Flask/flaskblog/__init__.py
+++ CoreyS_Flask/flaskblog/__init__.py
@@ -1,14 +1,24 @@
 
+import os
 from flask import Flask
-from flaskblog import routes
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+
+app.config['MAIL_USERNAME'] = os.environ.get('USER_EMAIL')
+app.config['MAIL_PASSWORD'] = os.environ.get('USER_PASSWORD')
+
+mail = Mail(app)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -16,4 +26,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+from flaskblog import routes
 
